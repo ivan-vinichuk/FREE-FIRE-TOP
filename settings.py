eefe49a00d100b9b9c9e23e614de24a19895fcf7
ivan-vinichuk <81975196+ivan-vinichuk@users.noreply.github.com>
2022-04-26 10:37:33 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,18 @@
+
+token = "Token"
+
+admins = [id] #админы бота через запятую
+
+LITTLE_SUBCOIN_TO_GET_SUBS = 5 #минимум сабкоинов для раскрутки канала
+
+REF_BONUS = 1 #реферальный бонус (сабкоины)
+
+FINE_FOR_UNSUBSCRIBING = -5 #Число должно быть отрицательным и целым
+
+SUBSCRIPTION_TERM = 7 #Какое время юзер должен оьязательно быть подписан на канал?
+
+LINK_TO_INTRODUCTION_AND_RULES = 'https://telegra.ph/Vvedenie-v-bota-a-takzhe-pravila-06-09' #ссылка на правила и инструкцию
+
+LINK_TO_CHAT_OF_BOT = 'link_group' #ссылка на чат
+
+chat_id = 'id_chat'
